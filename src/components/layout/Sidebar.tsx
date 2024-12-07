a5905adf0277d169e1bb537e59ea622fa00b1d80
Nizam <nushohel2@gmail.com>
2024-12-07 09:41:20 +0600

    git pulled

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -3,7 +3,7 @@ import { FaAngleDown } from 'react-icons/fa';
 function Sidebar() {
   return (
     <div className="max-w-56 bg-regal-blue pt-7 h-screen text-center">
-      <div className="flex flex-col items-center mx-4 leading-10 border-dashed border-b border-white ">
+      <div className="flex flex-col items-center mx-4 leading-10 border-dashed cursor-pointer border-b border-white ">
         <FaShop className="size-10 bg-white p-2 rounded-full" />
         <h1 className="text-white text-2xl font-bold text-[16px] flex items-center gap-2 ">
           New Molla & Sons <FaAngleDown className="size-4" />
